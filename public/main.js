3c099084e53165aa11340d4841b971f60501a084
eren--yeager <152408212+mith-ran@users.noreply.github.com>
2025-04-05 09:40:57 +0530

    Update main.js io

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-import { io } from 'https://cdn.socket.io/4.8.1/socket.io.esm.min.js';
+import { io } from 'https://cdn.socket.io/4.5.4/socket.io.esm.min.js';
 
 let currentContact = null;
 const chatHistory = {}; // Stores messages per contact
